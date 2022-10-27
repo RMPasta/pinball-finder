e43703ab9f9c83a4cda962b3d22cd4492978811d
Ryan Malmos <rmpasta.code@gmail.com>
2022-10-27 09:54:11 -0400

    add input value submission for api call. relatively complete application now.

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import "./App.css";
 import { v4 as uuidv4 } from "uuid";
 
 function App() {
-  //using this state to display pinball locations
+  //using this state to store and display pinball locations
   const [pinball, setPinball] = useState([]);
-  //trying to make references to the input boxes
+  //make references to the input boxes
   const longitudeRef = useRef(null);
   const latitudeRef = useRef(null);
 
+  //this one auto finds lat and lon and searches 50 mile radius
   const getNearestPinball = () => {
     //on success, set lon and lat to the coordinates from navigator.geolocation.getCurrentPosition
     const successCallback = (position) => {
-      const lon = position.coords.longitude;
       const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+      latitudeRef.current.value = position.coords.latitude;
+      longitudeRef.current.value = position.coords.longitude;
       //api call
       axios
         .get(
@@ -22,13 +25,9 @@ function App() {
         )
         .then((response) => {
           //set response to variable and set the state to that variable
-          //added in street, city etc and now it wont complete call due to multiple responses
-          //so im trying to map over every location in response and display them...
-
           setPinball(response.data.locations);
         });
     };
-
     const errorCallback = (error) => {
       console.log(error);
     };
@@ -36,14 +35,39 @@ function App() {
     navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
   };
 
-  //the idea is to have the inputs for lon and lan go to the api call on the click of a button
+  //the idea is to have the inputs for lat and lon go to the api call on the click of a button
   //or if they dont enter anything it will ask for their location and if agreed will grab results
   //right now only the button works and only for one result
+
+  const handleFind = () => {
+    //grabbing input from text inputs and assigning them to variables
+    const lat = latitudeRef.current.value;
+    const lon = longitudeRef.current.value;
+    //api call
+    axios
+      .get(
+        `https://pinballmap.com/api/v1/locations/closest_by_lat_lon.json?lat=${lat}&lon=${lon}&send_all_within_distance=true`
+      )
+      .then((response) => {
+        //set response to variable and set the state to that variable
+        setPinball(response.data.locations);
+      });
+  };
+
   return (
     <div className="App">
-      <input type="text" ref={longitudeRef} />
-      <input type="text" ref={latitudeRef} />
-      <button onClick={getNearestPinball}>Where's the pinball?!</button>
+      <label>
+        Latitude:
+        <input type="text" ref={latitudeRef} />
+      </label>
+      <label>
+        Longitude:
+        <input type="text" ref={longitudeRef} />
+      </label>
+      <button onClick={handleFind}>Find!</button>
+      <div className="button-wrapper">
+        <button onClick={getNearestPinball}>Auto Find Near Me</button>
+      </div>
       <ul>
         {pinball.map((locations) => (
           <li key={uuidv4()}>
